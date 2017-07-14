ad1cc7683bf5ca2a7a4094f3c8ffd4386eeabe08
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2017-07-14 19:44:34 +0100

    [TID] Add initial files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter_Sweep_Data_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = @"C:\Users\Tim\Dropbox\Phd\Publication\Field Spread\parameter sweep data";
+            string[] files = System.IO.Directory.GetFiles(folder);
+
+            foreach (string filename in files)
+            {
+                string name = System.IO.Path.GetFileName(filename);
+                name = name.Remove(name.Length - 8);
+            }
+            int i = 0;
+        }
+    }
+}
